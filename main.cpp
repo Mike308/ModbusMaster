6ef5885983d7e07ebaca4790eda8041d272f9511
Majkel44 <m.werner9205@gmail.com>
2017-02-18 18:53:32 +0100

    - added example

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
+#include "modbus.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Modbus mb;
+
+    mb.connectToSlave("COM2",19200);
+    mb.executeReadRequest(10,100,4);
 
     return a.exec();
 }
